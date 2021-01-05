11c67ae4d77d85c0525bb607fe698b9e3109027c
valerie <ooakhu@gmail.com>
2021-01-05 21:15:14 +0100

    changes

--- tailor_project/settings.py
+++ tailor_project/settings.py
@@ -161,6 +161,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
